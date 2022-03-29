67a550c555abe19dcb1ac4e3c545a73e745d6f51
dima.v <dima.v@genolis.com.au>
2022-03-29 08:53:48 +0300

    chore: debugger commented

--- packages/cosmwasm/test/tasks/example-lcd-client-extra.js
+++ packages/cosmwasm/test/tasks/example-lcd-client-extra.js
@@ -1,14 +1,14 @@
-const { task } = require("@iboss/terrain");
+const { task } = require('@iboss/terrain');
 
 task(async ({ wallets, refs, config, client }) => {
   // query is a thin wrapper of contract query
-  const count = await client.query("counter", { get_count: {} });
-  console.log("prev count = ", count);
+  const count = await client.query('counter', { get_count: {} });
+  console.log('prev count = ', count);
 
   // execute is a thin wrapper of signing and broadcasting execute contract
-  await client.execute(wallets.validator, "counter", {
+  await client.execute(wallets.validator, 'counter', {
     increment: {},
   });
-  const count2 = await client.query("counter", { get_count: {} });
-  console.log("new count = ", count2);
+  const count2 = await client.query('counter', { get_count: {} });
+  console.log('new count = ', count2);
 });

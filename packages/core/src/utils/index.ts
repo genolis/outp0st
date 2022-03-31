162a75788d4f9da93fc6274e230f098f19152937
Dima v <dima.v@genolis.com.au>
2022-03-31 12:36:46 +0300

    feat: full cycle - rover -> payload in one command, first version

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -1,12 +1,19 @@
+import { OutpostCurrentState } from 'types';
+
 export function GetId() {
   return Date.now() + new Date().getMilliseconds();
 }
 
 export function getTabTitle(title: string) {
-  console.log('tst');
   return title.slice(0, 3).toUpperCase();
 }
 
-export function test(title: string) {
-  console.log('tst');
+export function getDefaultGlobalState(title = 'Default Outpost payload title') {
+  return {
+    title: title,
+    current: OutpostCurrentState.LOCAL,
+    local: { contracts: [], messages: [] },
+    test: { contracts: [], messages: [] },
+    main: { contracts: [], messages: [] },
+  };
 }

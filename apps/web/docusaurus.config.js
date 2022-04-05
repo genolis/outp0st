94f3f3392e1970ffbc56d22b3aa59425a25ce78d
Dima v <dima.v@genolis.com.au>
2022-04-06 01:41:41 +0300

    chore: web build fix

--- apps/web/docusaurus.config.js
+++ apps/web/docusaurus.config.js
@@ -21,7 +21,6 @@ module.exports = {
     [
       'docusaurus2-dotenv',
       {
-        path: './.env', // The path to your environment variables.
         safe: false, // If false ignore safe-mode, if true load './.env.example', if a string load that file as the sample
         systemvars: true, // Set to true if you would rather load all system variables as well (useful for CI purposes)
         silent: false, //  If true, all warnings will be suppressed

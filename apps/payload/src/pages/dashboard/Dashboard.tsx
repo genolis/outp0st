04e3a258c99fe0d3acafb3cd1cb8d7b751a98f5e
Dima v <dima.v@genolis.com.au>
2022-04-05 11:48:29 +0300

    chore(payload): redirect from dashboard to OP

--- apps/payload/src/pages/dashboard/Dashboard.tsx
+++ apps/payload/src/pages/dashboard/Dashboard.tsx
@@ -1,17 +1,23 @@
-import { useTranslation } from "react-i18next"
-import { Col, Page } from "components/layout"
-import LunaPrice from "./LunaPrice"
-import Issuance from "./Issuance"
-import CommunityPool from "./CommunityPool"
-import StakingRatio from "./StakingRatio"
-import Charts from "./Charts"
-import styles from "./Dashboard.module.scss"
+import { Col, Page } from 'components/layout';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import Charts from './Charts';
+import CommunityPool from './CommunityPool';
+import styles from './Dashboard.module.scss';
+import Issuance from './Issuance';
+import LunaPrice from './LunaPrice';
+import StakingRatio from './StakingRatio';
 
 const Dashboard = () => {
-  const { t } = useTranslation()
+  const { t } = useTranslation();
+  let navigate = useNavigate();
+  useEffect(() => {
+    navigate('/outpost');
+  }, []);
 
   return (
-    <Page title={t("Dashboard")}>
+    <Page title={t('Dashboard')}>
       <Col>
         <header className={styles.header}>
           <LunaPrice />
@@ -23,7 +29,7 @@ const Dashboard = () => {
         <Charts />
       </Col>
     </Page>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;

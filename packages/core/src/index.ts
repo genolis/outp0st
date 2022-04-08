2df6ad2bae6b4fd67aa04cd3358abef704748476
Dima v <dima.v@genolis.com.au>
2022-04-08 10:02:26 +0300

    chore: #16

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,11 +1,3 @@
-export function foo(a: number, b: number): number {
-  return a + b;
-}
-
-export function bar(a: number, b: number): number {
-  return a - b;
-}
-
 export * from './state';
 export * from './types';
 export * from './utils';

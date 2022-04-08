290ea5066409b3a78beb84d5f7dc456b7eb4070c
Dmitriy Rodygin <darodygin@greenatom.ru>
2022-04-08 17:56:05 +0300

    feat: check if tab exists

--- apps/payload/src/outpost/components/OutpostTabs.tsx
+++ apps/payload/src/outpost/components/OutpostTabs.tsx
@@ -1,3 +1,5 @@
+import MuiAlert from '@mui/material/Alert';
+import Snackbar from '@mui/material/Snackbar';
 import { Contract } from '@outp0st/core';
 import { Form, FormItem, Input } from 'components/form';
 import { Button } from 'components/general';
@@ -31,7 +33,7 @@ export function OutpostTabs() {
       key: 'NEW',
       tab: '+',
       disabled: ro,
-      children: GetNewTab(),
+      children: GetNewTab(tabs),
     },
   ];
   const finalTabs = withConfig.concat(withAdd);
@@ -74,31 +76,43 @@ function GetMessages(contract: Contract) {
     );
   });
 }
-function GetNewTab() {
+function GetNewTab(tabs: { key: string; tab: string }[]) {
   const [state, setState] = useState({ tabTitle: '', title: '' });
+  const [isWarningOpen, setWarningOpen] = useState(false);
   const navigate = useNavigate();
   const { addContract } = useOutpostState();
+
+  const onCloseWarning = (_, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setWarningOpen(false);
+  };
+
+  const onAdd = () => {
+    const isTabExist = !!tabs.find(i => i.tab === state.tabTitle);
+
+    if (isTabExist) {
+      setWarningOpen(true);
+    } else {
+      addContract({
+        title: state.title,
+        id: 0,
+        tabTitle: state.tabTitle,
+        messages: [],
+      });
+      navigate(
+        { hash: state.tabTitle.replaceAll(' ', '_') },
+        { replace: true },
+      );
+    }
+  };
 
   return (
     <Card
       title="Create new tab"
       extra={
-        <Button
-          disabled={!state.title || !state.tabTitle}
-          onClick={() => {
-            addContract({
-              title: state.title,
-              id: 0,
-              tabTitle: state.tabTitle,
-              messages: [],
-            });
-
-            navigate(
-              { hash: state.tabTitle.replaceAll(' ', '_') },
-              { replace: true },
-            );
-          }}
-        >
+        <Button disabled={!state.title || !state.tabTitle} onClick={onAdd}>
           Create
         </Button>
       }
@@ -124,6 +138,17 @@ function GetNewTab() {
           />
         </FormItem>
       </Form>
+
+      <Snackbar
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        open={isWarningOpen}
+        autoHideDuration={3000}
+        onClose={onCloseWarning}
+      >
+        <MuiAlert onClose={onCloseWarning} severity="warning">
+          Tab with the same name already exists!
+        </MuiAlert>
+      </Snackbar>
     </Card>
   );
 }

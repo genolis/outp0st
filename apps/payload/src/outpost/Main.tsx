cb25f2c3ed36e2f79c5d82560f3798ce909ab509
Dmitriy Rodygin <darodygin@greenatom.ru>
2022-04-11 21:25:25 +0300

    fix: indicator that state is loading from web3

--- apps/payload/src/outpost/Main.tsx
+++ apps/payload/src/outpost/Main.tsx
@@ -1,17 +1,38 @@
-import { Button } from '@mui/material';
+import { Button, Skeleton } from '@mui/material';
 import { OutpostCurrentState } from '@outp0st/core';
+import classNames from "classnames/bind";
 import { ExtraActions, Page } from 'components/layout';
 import { OutpostTabs } from './components/OutpostTabs';
 import BtnGroup from './components/ui/elements/BtnGroup';
 import Tabs from './components/ui/elements/Tabs';
 import { useStartup } from './hooks/useStartup';
+import styles from './Main.module.scss';
+import { useOutpostSettings } from './state/useOutpostSettings';
 import { useOutpostState } from './state/useOutpostState';
 
-const Lunaverse = () => {
-  useStartup();
-  const { outpostGlobal, outpost, switchCurrentState } = useOutpostState();
+const Loading = (props) => {
+  const { hide, show } = styles;
 
-  return (
+  return (<>
+    {props.loading && <Page>
+      <Skeleton animation="wave" variant="rectangular" height={55}></Skeleton>
+      <Skeleton animation="wave" height={100}></Skeleton>
+      <Skeleton animation="wave" variant="rectangular" height={900}></Skeleton>
+    </Page >}
+    <div className={classNames(props.loading && hide, show)}>
+      {props.children}
+    </div>
+  </>)
+}
+
+
+const Lunaverse = () => {
+  const { outpostGlobal, outpost, switchCurrentState } = useOutpostState();
+  const { loading } = useOutpostSettings();
+
+  useStartup();
+
+  return <Loading loading={loading}>
     <Page
       title={outpost.title ? outpost.title : 'Outpost admin'}
       extra={
@@ -33,11 +54,10 @@ const Lunaverse = () => {
             ))}
           </BtnGroup>
         </ExtraActions>
-      }
-    >
+      }>
       <Tabs tabs={OutpostTabs()} type="card" />
     </Page>
-  );
+  </Loading>
 };
 
 export default Lunaverse;
